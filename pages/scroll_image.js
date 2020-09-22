8c6b193b9ada8b8658ce976269ffbdeb5b176530
Defragmentator277 <denzimin1@gmail.com>
2020-09-22 11:44:54 +0300

    sdfsfd

--- pages/scroll_image.js
+++ pages/scroll_image.js
@@ -1,94 +1,85 @@
 window.onload = () => 
 {
     let active_img = 1;
-    let scroll_box = getElem('scroll_box');
-    let arrow = getElem('info_expande');
-    let reviev = getElem('text_box');
-    let rect = scroll_box.getBoundingClientRect();
-    arrow.onclick = (e) => 
+    let animated_ = true;
+    let Y;
+    let element = $('#img_scroll_' + active_img);
+    let scroll_box = $('#scroll_box_albums');
+    let arrow = $('#info_expande');
+    let reviev = $('#text_box');
+    reviev.slideUp();
+    arrow.on("click", (e) => 
     {
-        if(!arrow.style.transform)//expande
+        if(arrow.data('orientation') == 'down')
         {
-            arrow.style.transform = 'rotate(180deg)';
-            reviev.style.opacity = '1';
-            reviev.style.height = '500px';
-        }
-        else//shrink
-        {
-            reviev.style.opacity = '0';
-            reviev.style.height = '0px';
-            arrow.style.transform = '';
-        }
-    }
-    scroll_box.onclick = function(e) 
-    {
-        element = getElem('img_scroll_' + active_img);
-        let elem;
-        let to_hide;
-        let to_show;
-        //left
-        if(rect.width / 2 + rect.left > e.clientX)
-        {
-            elem = getElem('img_scroll_' + (active_img - 1));
-            if(elem)//On middle
-            {
-                to_hide = getElem('img_scroll_' + (active_img + 1));
-                to_show = getElem('img_scroll_' + (active_img - 2));
-                active_img--;
-                element.style.left = '100%';
-                element.style.marginLeft = '-300px';
-                element.style.width = '300px';
-                element.style.opacity = '0.5';
-                element.style.zIndex = '0';
-                //
-                // elem.style.right = '25%';
-                elem.style.left = '50%';
-                elem.style.marginLeft = '-250px';
-                elem.style.width = '500px';
-                elem.style.opacity = '1';
-                elem.style.zIndex = '1';
-            }
-
+            arrow.css('transform', 'rotate(180deg)');
+            arrow.css('display', 'blocks');
+            arrow.data('orientation', 'up');
+            //
+            // reviev.fadeTo(400, 1);
+            reviev.slideDown();
         }
         else
         {
-            console.log('right');
-            elem = getElem('img_scroll_' + (active_img + 1));
-            if(elem)//On middle
+            arrow.css('transform', 'rotate(0deg)');
+            arrow.data('orientation', 'down');
+            //
+            reviev.slideUp();
+        }
+    });
+    scroll_box.on('click', (e) =>
+    {
+        let koeff = -1;
+        element = $('#img_scroll_' + active_img);
+        //left
+        if(element.offset().left + element.width() / 2 > e.clientX)
+            koeff = 1;
+        let elem = $('#img_scroll_' + (active_img - 1 * koeff));
+        if(elem.length && animated_)//
+        {
+            let to_hide = $('#img_scroll_' + (active_img + 1 * koeff));
+            let to_show = $('#img_scroll_' + (active_img - 2 * koeff));
+            let left = '0';
+            let margin_left = '0';
+            let box_shadow = '-20px';
+            active_img -= koeff;
+            if(koeff == 1 && to_hide && to_show)//left
             {
-                to_hide = getElem('img_scroll_' + (active_img - 1));
-                to_show = getElem('img_scroll_' + (active_img + 2));
-                active_img++;
-                element.style.left = '0';
-                element.style.marginLeft = '0';
-                element.style.width = '300px';
-                element.style.opacity = '0.5';
-                element.style.zIndex = '0';
-                //
-                // elem.style.left = '25%';
-                elem.style.left = '50%';
-                elem.style.width = '500px';
-                elem.style.marginLeft = '-250px';
-                elem.style.opacity = '1';
-                elem.style.zIndex = '1';
-            }
+                left = '100%';
+                margin_left = '-300px';
+                box_shadow = '20px';
+            }//right
+            animated_ = false;
+            element.animate({ 'opacity': '0.5', 'z-index': '0', 'left': left, 'width': '300px' }, {complete: () => 
+            {
+                element.css('box-shadow', box_shadow + ' -20px 30px 5px rgba(0, 0, 0, 1)');
+                element.animate({ 'margin-left': margin_left }, 1000);
+                to_hide.fadeOut();
+                elem.animate({ 'opacity': '1', 'z-index': '1', 'left': '50%', 'width': '500px'}, {complete: () => 
+                {
+                    elem.css('box-shadow', '0px -10px 30px 5px rgba(0, 0, 0, 1)');
+                    elem.animate({ 'margin-left': '-250px' });
+                    to_hide.hide('fast');
+                    to_show.show();
+                    animated_ = true;
+                }});
+            }}); 
         }
-        if(to_hide)
+    }).on('mousedown', (e) => 
+    {
+        // Y = e.clientY;
+        animated_ = true;
+        Y = e.clientY;
+    }).on('mousemove', (e) => 
+    {
+        element = $('#img_scroll_' + active_img);
+        if(animated_)
         {
-            console.log('to_hide - ' + to_hide.style.display);
-            to_hide.style.display = 'none';
+            element.offset({ 'top': Y - e.clientY});
         }
-        if(to_show)
-        {
-            console.log('to_show - ' + to_show.style.display);
-            to_show.style.display = 'block';
-        }
-    }
+    }).on('mouseup', (e) => 
+    {
 
-}
+    });
 
-
-function getElem(id)
-{
-    return document.getElementById(id);
 }
